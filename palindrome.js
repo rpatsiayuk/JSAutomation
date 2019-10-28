848052ff47e31fd257915edb5bc08f8c9ee78380
Raman Patsiayuk <Raman.Patsiayuk@verint.com>
2019-10-28 11:58:00 +0300

    added palindrome and updateSence js functions

--- palindrome.js
+++ palindrome.js
@@ -1,16 +1,14 @@
 function checkStringIsPalindrome(text) {
-    if (typeof text !== 'string' || text == 'undefined' || !text) {
+    if (typeof text !== 'string' || !text) {
         console.log("Parameter is invalid = " + text)
+        return false
     } else {
         // Split text into array of characters
         let charArray = text.toUpperCase().split('');
         //reverse array 
         let result = charArray.reverse().join('')
-        if (text.toUpperCase() === result) {
-            console.log("String is palindrome " + text)
-        }
+        return text.toUpperCase() === result
     }
-    console.log("String is not a palindrome " + text)
 }
 
 console.log(checkStringIsPalindrome('Anna'))
